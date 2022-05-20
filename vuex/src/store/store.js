39d82f4c9fe07c976445b9ba1879dd498eb5f7a1
Leonardo <leonardo.amaral92@gmail.com>
2022-05-20 13:38:55 -0300

    Centralizando cálculo do total da compra usando getter

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -6,5 +6,13 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         produtos: []
+    },
+    getters: {
+        //Centralizamos a lógica que existia nos componentes Resumo e Carrinho aqui
+        //Desta forma só precisamos alterar em um lugar algo que pode ser usado em vários lugares        
+        valorTotal(state){
+            return state.produtos.map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0)
+        }
     }
 })
